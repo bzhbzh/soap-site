537227a3aefa6a015236e94230346379026d7b78
Ryzomych <rizoma@mail.riri>
2020-03-22 11:52:01 +0300

    chenged block and images

--- php/mail.php
+++ php/mail.php
@@ -16,12 +16,12 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'team@falanster.by'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "BZH-BZH Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$to = 'team@geeklauka.by'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Замова з Мыльніцы гік-лаўкі:  $name";
+$email_body = "Вы атрымалі замову\r\n"."Дэталі:\r\nІмя: $name\n\nEmail: $email_address\n\nТэлефон: $phone\n\nЗамова:\n$message";
 
 
-$headers = "From: falanster.by@gmail.com\r\n";
+$headers = "From: $email_address\r\n";
 $headers.= "MIME-Version: 1.0\r\n";
 $headers.= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 $headers.= "X-Priority: 1\r\n";
